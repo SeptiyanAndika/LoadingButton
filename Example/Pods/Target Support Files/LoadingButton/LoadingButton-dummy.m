157d5965af36d06543324291da4a9bfe89b6ba1f
Septiyan Andika Isanta <septiyan.andika@gmail.com>
2016-06-26 18:33:22 +0700

    Initial Commit

---
+++ Example/Pods/Target Support Files/LoadingButton/LoadingButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LoadingButton : NSObject
+@end
+@implementation PodsDummy_LoadingButton
+@end
---
+++ Example/Pods/Target Support Files/Pods-LoadingButton_Tests/Pods-LoadingButton_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoadingButton_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LoadingButton_Tests
+@end
